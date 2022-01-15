1793e9dffe66807c8b08dcc53142957dabde0e95
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-15 15:18:38 -0300

    CreateContract Camada Controllers

--- server/controllers/createContract.js
+++ server/controllers/createContract.js
@@ -1,3 +1,11 @@
-module.exports = (req,res) => {
-    res.status(200).send("OK")
+const servicesCreateContract = require('../services/createContract');
+
+module.exports = async (req,res) => {
+    const createContract = await servicesCreateContract(req.body);
+
+    if (createContract.err) {
+      return res.status(401).json(createContract)
+    }
+
+    return createContract;
 };
