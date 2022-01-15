5b7e04d4f070b1e332aee4f4ed30806eb2eee7cc
Carol_Inglethe <carolinainglethe@gmail.com>
2022-01-15 15:03:26 -0300

    altera create-db e altera getContracts models

--- server/models/getContracts.js
+++ server/models/getContracts.js
@@ -3,6 +3,8 @@ const Contracts = require('./schemas-model/contracts');
 module.exports = async () => {
 
   const getContracts = await Contracts.find()
+    .then((findContracts) => findContracts)
+    .catch((e) => null)
 
   return getContracts;
 
